81403973cada93ab60c6e61bda9261b5f771df1a
ChetanSawant <chetansawant.dev@gmail.com>
2024-07-14 16:07:45 +0530

    Initial Commit for Quality Module

---
+++ src/main/java/com/plantbizquality/PlantBizQualityApplication.java
@@ -0,0 +1,13 @@
+package com.plantbizquality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantBizQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantBizQualityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantbizquality/PlantBizQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantbizquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantBizQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
